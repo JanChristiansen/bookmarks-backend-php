5c077ed86c066065ecdaa5afdf7c50126c892328
Jan Christiansen <jan.christiansen@myhammer.net>
2016-11-07 22:24:28 +0100

    killed duplicate code

--- src/AppBundle/DataFixtures/ORM/LoadFullTreeCategoriesData.php
+++ src/AppBundle/DataFixtures/ORM/LoadFullTreeCategoriesData.php
@@ -28,45 +28,25 @@ class LoadFullTreeCategoriesData extends AbstractFixture implements OrderedFixtu
         /** @var Category[] $children1 */
         $children1 = array();
         for ($i = 1; $i <= 4; $i++) {
-            $name = 'category-1' . $i;
-            $children1[$i] = new Category();
-            $children1[$i]->setName($name);
-            $children1[$i]->setParent($root);
-            $manager->persist($children1[$i]);
-            $this->addReference('full-tree-' . $name, $children1[$i]);
+            $children1[$i] = $this->createChildCategory($manager, 'category-1' . $i, $root);
         }
 
         /** @var Category[] $children12 */
         $children12 = array();
         for ($i = 1; $i <= 3; $i++) {
-            $name = 'category-12-1' . $i;
-            $children12[$i] = new Category();
-            $children12[$i]->setName($name);
-            $children12[$i]->setParent($children1[2]);
-            $manager->persist($children12[$i]);
-            $this->addReference('full-tree-' . $name, $children12[$i]);
+            $children12[$i] = $this->createChildCategory($manager, 'category-12-1' . $i, $root);
         }
 
         /** @var Category[] $children13 */
         $children13 = array();
         for ($i = 1; $i <= 3; $i++) {
-            $name = 'category-13-1' . $i;
-            $children13[$i] = new Category();
-            $children13[$i]->setName($name);
-            $children13[$i]->setParent($children1[3]);
-            $manager->persist($children13[$i]);
-            $this->addReference('full-tree-' . $name, $children13[$i]);
+            $children13[$i] = $this->createChildCategory($manager, 'category-13-1' . $i, $root);
         }
 
         /** @var Category[] $children131 */
         $children131 = array();
         for ($i = 1; $i <= 2; $i++) {
-            $name = 'category-13-11-1' . $i;
-            $children131[$i] = new Category();
-            $children131[$i]->setName($name);
-            $children131[$i]->setParent($children13[1]);
-            $manager->persist($children131[$i]);
-            $this->addReference('full-tree-' . $name, $children131[$i]);
+            $children131[$i] = $this->createChildCategory($manager, 'category-13-11-1' . $i, $root);
         }
 
         $manager->flush();
@@ -77,5 +57,23 @@ class LoadFullTreeCategoriesData extends AbstractFixture implements OrderedFixtu
         // the order in which fixtures will be loaded
         // the lower the number, the sooner that this fixture is loaded
         return 99;
+    }
+
+    /**
+     * @param ObjectManager $manager
+     * @param $name
+     * @param $root
+     * @return Category
+     */
+    protected function createChildCategory(ObjectManager $manager, $name, $root)
+    {
+        $category = new Category();
+        $category->setName($name);
+        $category->setParent($root);
+
+        $manager->persist($category);
+        $this->addReference('full-tree-' . $name, $category);
+
+        return $category;
     }
 }
